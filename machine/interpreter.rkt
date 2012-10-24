3733486bd9c7060a4c544deb06eac5ac9b5581e8
Tikhon Jelvis <tikhon@berkeley.edu>
2012-10-24 11:23:01 -0700

    The initial version of the instruction-level interpreter.

---
+++ machine/interpreter.rkt
@@ -0,0 +1,134 @@
+;;; A bit-level arrayForth interpreter.
+#lang racket
+
+;;; For convenience, I'm going to pretend racket integers are 18 bits
+;;; wide. 
+
+;;; stacks:
+(struct stack ([sp #:mutable] body))
+(define data   (stack 0 (make-vector 8)))
+(define return (stack 0 (make-vector 8)))
+
+;;; registers:
+(define a 0)
+(define b 0)
+(define p 0)
+(define i 0)
+(define r 0)
+(define s 0)
+(define t 0)
+
+;;; Extracts the bottom 18 bits of n:
+(define (18bit n)
+  (bitwise-bit-field n 0 18))
+
+;;; Pushes a value to the given stack's body.
+(define (push-stack! stack value)
+  (set-stack-sp! stack (modulo (add1 (stack-sp stack)) 8))
+  (vector-set! (stack-body stack) (stack-sp stack) s))
+
+;;; Pushes to the data stack.
+(define (push! value)
+  (push-stack! data s)
+  (set! s t)
+  (set! t (18bit value)))
+
+;;; Pushes to the return stack.
+(define (r-push! value)
+  (push-stack! return r)
+  (set! r value))
+
+;;; Pops from the given stack's body.
+(define (pop-stack! stack)
+  (let ([ret-val (vector-ref (stack-body stack) (stack-sp stack))])
+    (set-stack-sp! stack (modulo (sub1 (stack-sp stack)) 8))
+    ret-val))
+
+;;; Pops from the data stack.
+(define (pop!)
+  (let ([ret-val t])
+    (set! t s)
+    (set! s (pop-stack! data))
+    ret-val))
+
+;;; Pops from the return stack.
+(define (r-pop!)
+  (let ([ret-val r])
+    (set! r (pop-stack! return))
+    ret-val))
+
+(define memory (make-vector 64))
+
+(define instructions (make-vector 32))
+
+;;; Executes a single integer, treating it as an 18-bit word.
+(define (execute-word!)
+  (define (execute! opcode jump-addr-pos)
+    (let ([jump-addr (bitwise-bit-field 0 jump-addr-pos)])
+      ((vector-ref instructions opcode) jump-addr)))
+  (and (execute! (bitwise-bit-field i 13 18) 10)
+       (execute! (bitwise-bit-field i 8 13)  8)
+       (execute! (bitwise-bit-field i 3 8)   3)
+       (execute! (bitwise-bit-field i 0 3)   0)))
+
+;;; Return the value of p or a incremented as appropriately. If the
+;;; register points to an IO region, does nothing. Otherwise increment
+;;; the register circularly within the current memory region (RAM or
+;;; ROM).
+(define (incr curr)
+  (cond [(< curr #x07F) (add1 curr)]
+        [(= curr #x07F) #x000]
+        [(< curr #x0FF) (add1 curr)]
+        [(= curr #x0FF) #x080]
+        [else curr]))
+
+;;; Executes a program starting with the given memory address. This
+;;; executes a word, increasing i and p as appropriate.
+(define (execute-program! start)
+  (set! i (vector-ref memory p))
+  (execute-word!)
+  (set! p (incr p)))
+
+;;; Defines a new instruction. This implicitly sets the instructions'
+;;; opcodes based on the order they're defined in. An instruction can
+;;; abort the rest of the current word by returning #f.
+(define define-instruction!
+  (let ([current-opcode 0])
+    (lambda (body)
+      (vector-set! instructions current-opcode body)
+      (set! current-opcode (add1 current-opcode)))))
+
+(define-instruction! (lambda (_) (set! p r) (r-pop!) #f))                            ; return (;)
+(define-instruction! (lambda (_) (define temp p) (set! p r) (set! r temp) #f))       ; execute (ex)
+(define-instruction! (lambda (a) (set! p a) #f))                                     ; jump (name ;)
+(define-instruction! (lambda (a) (r-push! p) (set! p a) #f))                         ; call (name)
+(define-instruction! (lambda (_) (if (= r 0) (r-pop!)                                ; micronext (unext) -- hacky!
+                           (begin (set! r (sub1 r)) (set! p (sub1 p)) #f))))
+(define-instruction! (lambda (a) (if (= r 0) (begin (r-pop!) #f)                     ; next (next)
+                           (begin (set! r (sub1 r)) (set! p a) #f))))
+(define-instruction! (lambda (a) (and (not (= t 0)) (set! p a) #f)))                 ; if (if)
+(define-instruction! (lambda (a) (and (not (bitwise-bit-set? t 17)) (set! p a) #f))) ; minus if (-if)
+(define-instruction! (lambda (_) (push! (vector-ref memory p)) (set! p (incr p))))   ; fetch-p (@p)
+(define-instruction! (lambda (_) (push! (vector-ref memory a)) (set! a (incr a))))   ; fetch-plus (@+)
+(define-instruction! (lambda (_) (push! (vector-ref memory b))))                     ; fetch-b (@b)
+(define-instruction! (lambda (_) (push! (vector-ref memory a))))                     ; fetch (@)
+(define-instruction! (lambda (_) (vector-set! memory p (pop!)) (set! p (incr p))))   ; store-p (!p)
+(define-instruction! (lambda (_) (vector-set! memory a (pop!)) (set! a (incr a))))   ; store-plus (!+)
+(define-instruction! (lambda (_) (vector-set! memory b (pop!))))                     ; store-b (!b)
+(define-instruction! (lambda (_) (vector-set! memory a (pop!))))                     ; store (!)
+(define-instruction! (lambda (_) (void)))                                            ; TODO: multiply-step
+(define-instruction! (lambda (_) (set! t (18bit (arithmetic-shift t 1)))))           ; 2*
+(define-instruction! (lambda (_) (set! t (arithmetic-shift t -1))))                  ; 2/
+(define-instruction! (lambda (_) (set! t (18bit (bitwise-not t)))))                  ; not (-)
+(define-instruction! (lambda (_) (push! (+ (pop!) (pop!)))))                         ; TODO: extended arithmetic mode
+(define-instruction! (lambda (_) (push! (bitwise-and (pop!) (pop!)))))               ; and
+(define-instruction! (lambda (_) (push! (bitwise-xor (pop!) (pop!)))))               ; or
+(define-instruction! (lambda (_) (pop!)))                                            ; drop 
+(define-instruction! (lambda (_) (push! t)))                                         ; dup
+(define-instruction! (lambda (_) (push! (r-pop!))))                                  ; pop
+(define-instruction! (lambda (_) (push! s)))                                         ; over
+(define-instruction! (lambda (_) (push! a)))                                         ; read a (a)
+(define-instruction! (lambda (_) (void)))                                            ; nop (.)
+(define-instruction! (lambda (_) (r-push! (pop!))))                                  ; push
+(define-instruction! (lambda (_) (set! b (pop!))))                                   ; store into b (b!) 
+(define-instruction! (lambda (_) (set! a (pop!))))                                   ; store into a (a!)
