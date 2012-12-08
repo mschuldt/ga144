db0ccb37af1da5a2a761b91cdec8492aeeb9a9b3
Tikhon Jelvis <tikhon@jelv.is>
2012-12-08 15:03:00 -0800

    Made the tests runnable with different programs.

--- machine/performance.rkt
+++ machine/performance.rkt
@@ -13,26 +13,20 @@
   (/ (apply + args) (length args)))
 
 ;;; Test how the number of bits affects synthesis time.
-(define (bits)
+(define (bits program)
   (for/list ([bits (in-range 2 19)])
             (pretty-display bits)
             (cons bits
-                  (time (fastest-program "over over or nop a! and a nop or nop nop nop"
-                                         #:slots 8 #:constraint (constraint t)
-                                         #:num-bits bits #:inst-pool `no-fake)))))
+                  (time (fastest-program program #:num-bits bits)))))
 
-(define (instr-pools)
+(define (instr-pools program)
   (for/list ([instrs '(all no-fake no-mem no-mem-no-p no-fake-no-p)])
             (pretty-display instrs)
             (cons instrs
-                  (time (fastest-program "over over or nop a! and a nop or nop nop nop"
-                                         #:slots 8 #:constraint (constraint t)
-                                         #:inst-pool instrs)))))
+                  (time (fastest-program program #:inst-pool instrs)))))
 
-(define (constraints)
+(define (constraints program)
   (for/list ([constraints (list constraint-all (constraint t) (constraint s) (constraint s t))])
             (pretty-display constraints)
             (cons constraints
-                  (time (fastest-program "over over or nop a! and a nop or nop nop nop"
-                                         #:slots 8 #:constraint constraints
-                                         #:inst-pool `no-fake)))))
+                  (time (fastest-program program #:constraint constraints)))))
