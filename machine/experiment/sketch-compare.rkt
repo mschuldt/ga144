aaa12a1c609f20cd9902fd7a858b9b0366b69088
mangpo <mangpo@eecs.berkeley.edu>
2014-02-21 21:34:37 -0800

    new example for sketch-rosette-compare

--- machine/experiment/sketch-compare.rkt
+++ machine/experiment/sketch-compare.rkt
@@ -2,11 +2,12 @@
 
 (require "../cegis.rkt" "../state.rkt" "../../ArrayForth/arrayforth.rkt")
 
-(define (test program n)
+(define (test program n mem constraint)
   (define start (current-seconds))
   (cegis (compile-to-string program) 
-         #:constraint (constraint s t) #:slots n #:length-limit 100)
+         #:mem mem #:constraint constraint #:slots n #:length-limit 100)
   (pretty-display `(cegis-time ,(- (current-seconds) start))))
  
-;;; x | y
-(test "over over or a! and a or" 5)
+;(test "325 b! !b 277 b! !b 373 b! !b 469 b! !b" 14 1 (constraint memory r s t))
+(test "2 b! @b 277 b! !b 1 b! @b 277 b! !b" 9 3 (constraint memory s t))
+;(test "5 b! !b 373 b! @b 5 b! @b 277 b! !b" 10 6 (constraint memory s t))
