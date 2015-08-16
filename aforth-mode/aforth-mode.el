0727357dca66c914b98da3c8149bb0169d77cbef
mschuldt <mbschuldt@gmail.com>
2015-08-16 14:52:23 -0700

    fix font locking

--- aforth-mode/aforth-mode.el
+++ aforth-mode/aforth-mode.el
@@ -2,21 +2,23 @@
 
 (defvar aforth-mode-hook nil)
 
-(setq aforth-instructions-1 '( ";" "ret"
-			       "." "nop"
-			       "ex" "jump" "call" "unext" "next" "if"
-			      "-if" "@p" "@b" "@" "!p"  "!b" "!"
-			      "-" "+" "and" "or" "drop" "dup" "pop"
-			      "over" "a" "push" "b!" "a!"))
+(setq aforth-instructions-1 '( "ret" "nop""ex" "jump" "call" "unext" "next" "if"
+			       "and" "or" "drop" "dup" "pop" "over" "a" "push" ))
 
-(setq aforth-instructions-2 '("@+" "!+" "+*" "2*" "2/"))
+(setq aforth-instructions-2 '("@+" "!+" "+*" "2*" "2/" "!p"  "!b" "!" "-if"
+                              ";" "." "-" "+"  "@p" "@b" "@" "b!" "a!"))
 
-(defvar aforth-ports '("up" "left" "down" "right" "io"))
+(setq aforth-ports '("up" "left" "down" "right" "io"
+                     "north" "east" "south" "west"))
 
-(defvar aforth-directives '("node" "org" ".." "start" "for" "begin"
-			    "then" "#swap" "while" "-while" "reclaim"
-			    ;;"," ":"
-			    ))
+(setq aforth-directives '( "start" "for" "begin" "then"
+                           "while"  "reclaim"))
+(setq aforth-directives2 '(".." "#swap" "-while" ","))
+
+;;directives that take an argument
+(setq aforth-directives-3 '("node" "org"))
+
+(setq boot-descriptors '("/b" "/a" "/io" "/p" "/stack"))
 
 (defun aforth-make-regexp (strings &optional word)
   (let ((x (concat "\\("
@@ -26,46 +28,75 @@
 	(concat "\\<" x "\\>")
       x)))
 
-(setq aforth-instruction-regexp-1 (aforth-make-regexp aforth-instructions t))
-(setq aforth-instruction-regexp-2 (aforth-make-regexp aforth-instructions))
+(setq aforth-number-regexp
+      "\\<\\(\\(0x[0-9a-fA-F]+\\)\\|\\(0b[01]+\\)\\|[0-9]+\\)\\>")
+(setq aforth-instruction-regexp-1 (aforth-make-regexp aforth-instructions-1 t))
+(setq aforth-instruction-regexp-2 (aforth-make-regexp aforth-instructions-2))
 (setq aforth-directive-regexp (aforth-make-regexp aforth-directives t))
+(setq aforth-directive-regexp-2 (aforth-make-regexp aforth-directives2))
 (setq aforth-ports-regexp (aforth-make-regexp aforth-ports t))
+(setq aforth-boot-descriptor-regex (aforth-make-regexp boot-descriptors))
+(setq aforth-directive-regexp-3
+      (concat "\\(" (mapconcat (lambda (x)
+                                 (format "%s[ ]+[a-zA-Z0-9]+" x))
+                               aforth-directives-3
+                               "\\|")
+              "\\)"))
 (aforth-make-regexp '("b!"))
 
-(defface aforth-instruction-face2 '((((background light)) (:foreground "green"))
-				 (((background dark)) (:foreground "green")))
+(defface aforth-instruction-face '((((background light)) (:foreground "green"))
+                                   (((background dark)) (:foreground "green")))
   "Default face for arrayforth instructions")
 
-(defface aforth-directive-face2 '((((background light)) (:foreground "yellow"))
-				 (((background dark)) (:foreground "yellow")))
+(defface aforth-directive-face '((((background light)) (:foreground "yellow"))
+                                 (((background dark)) (:foreground "yellow")))
   "Default face for arrayforth compiler directives")
 
-(defface aforth-number-face3 '((((background light)) (:foreground "LightGreen"))
-			       (((background dark)) (:foreground "LightGreen")))
+(defface aforth-number-face '((((background light)) (:foreground "LightGreen"))
+                              (((background dark)) (:foreground "LightGreen")))
   "Default face for arrayforth numbers")
 
-(defface aforth-word-face2 '((((background light)) (:foreground "red"))
-				 (((background dark)) (:foreground "red")))
+(defface aforth-word-face '((((background light)) (:foreground "red"))
+                            (((background dark)) (:foreground "red")))
   "Default face for arrayforth word definitions")
 
-(setq aforth-instruction-face 'aforth-instruction-face2)
-(setq aforth-directive-face 'aforth-directive-face2)
-(setq aforth-word-face 'aforth-word-face2)
-(setq aforth-number-face 'aforth-number-face3)
+(defface aforth-boot-descriptor-face '((((background light))
+                                        (:foreground "maroon"))
+                                       (((background dark))
+                                        (:foreground "maroon")))
+  "Default face for arrayforth boot descriptors")
+
+(defface aforth-word-reference-face '((((background light))
+                                       (:foreground "DeepPink"))
+                                      (((background dark))
+                                       (:foreground "DeepPink")))
+  "Default face for arrayforth boot descriptors")
+
+(setq aforth-instruction-face 'aforth-instruction-face)
+(setq aforth-directive-face 'aforth-directive-face)
+(setq aforth-word-face 'aforth-word-face)
+(setq aforth-number-face 'aforth-number-face)
+(setq aforth-boot-descriptor-face 'aforth-boot-descriptor-face)
+(setq aforth-word-reference-face 'aforth-word-reference-face)
+
+(setq aforth-indent-words '((("if" "begin" "for")    (0 . 2) (0 . 2))
+			    (("then" "next" "unext") (-2 . 0) (0 . -2))
+			    (("while" "-while")      (-2 . 4) (0 . 2))))
 
 (setq aforth-font-lock-keywords
       `(;;word definitions
 	("\\(:\\)[ \t\n]+\\([a-zA-Z0-9_-]+\\)"
 	 (1 aforth-word-face)
 	 (2 aforth-word-face))
-	;;numbers
-	;;TODO: should not color numbers inside of words
-	("\\(0x\\|0b\\)?[0-9+]" . aforth-number-face)
+        ("&[a-zA-Z0-9]+" . aforth-word-reference-face)
+	(,aforth-boot-descriptor-regex . aforth-boot-descriptor-face)
+        (,aforth-directive-regexp-2 . aforth-directive-face)
 	(,aforth-directive-regexp . aforth-directive-face)
-	(,aforth-instruction-regexp-1 . aforth-instruction-face)
+        (,aforth-directive-regexp-3 . aforth-directive-face)
 	(,aforth-instruction-regexp-2 . aforth-instruction-face)
+	(,aforth-instruction-regexp-1 . aforth-instruction-face)
 	(,aforth-ports-regexp . aforth-instruction-face)
-	;;font-lock-comment-face
+        (,aforth-number-regexp . aforth-number-face)
 	))
 
 (defvar aforth-mode-syntax-table
@@ -76,7 +107,7 @@
   "Syntax table in use in aforth buffers")
 
 (define-derived-mode aforth-mode prog-mode "aforth"
-  "Major mode to edit aforth"
+  "Major mode for editing aforth files"
   (setq font-lock-defaults '((aforth-font-lock-keywords)))
 
   (set-syntax-table aforth-mode-syntax-table)
