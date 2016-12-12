3a063bf9590a0f93bd7f4396c011aca7ad0ebc0f
mschuldt <mbschuldt@gmail.com>
2016-12-11 20:22:33 -0800

    more complete define macro

--- racket.el
+++ racket.el
@@ -14,6 +14,12 @@
              (push (cadr form) vars))
            (setq vars (append vars (racket-gather-local-vars (caddr form) exclude))))
 
+          ((and (eq (car form) 'define)
+                (consp (cadr form)))
+           (unless (member (cadr form) exclude)
+             (push (caadr form) vars))
+           (setq vars (append vars (racket-gather-local-vars (caddr form) exclude))))
+
           ((or (eq (car form) 'let)
                (eq (car form) 'let*))
            (setq vars (append vars (racket-gather-local-vars (caddr form)
@@ -23,30 +29,55 @@
                                                                              (cadr form)))))))
           (t (dolist (x form)
                (setq vars (append vars (racket-gather-local-vars x exclude))))))
-    vars))
+    (delete-dups vars)))
+
+
+(defun racket-translate-define-body (form &optional local-functions)
+  (cond ((not (consp form)) form)
+        ((eq (car form) 'define)
+         (if (consp (cadr form))
+             `(setq ,(caadr form) (lambda ,@(racket-make-define-body (cadr form)
+                                                                     (racket-translate-define-body (cddr form)
+                                                                                                   (cons (caadr form) local-functions)))))
+           (cons 'setq (racket-translate-define-body (cdr form) local-functions))))
+
+        ((member (car form) local-functions)
+         (cons 'funcall (cons (car form) (racket-translate-define-body (cdr form) local-functions))))
+
+        (t (let (body)
+             (dolist (x form)
+               (when (and (consp x)
+                          (eq (car x) 'define)
+                          (consp (cadr x)))
+                 (setq local-functions (cons (caadr x) local-functions)))
+               (push (racket-translate-define-body x local-functions) body))
+             (reverse body)))))
+
+
+(defun racket-make-define-body (form body)
+  (let ((args (cdr form))
+        local-vars name positional optional defaults)
+    (dolist (a args)
+      (if (vectorp a)
+          (progn
+            (assert (= (length a) 2))
+            (setq name (aref a 0))
+            (assert (symbolp name))
+            (push name optional)
+            (push `(setq ,name (or ,name ,(aref a 1))) defaults))
+        (assert (symbolp a))
+        (push a positional)))
+    (setq local-vars (racket-gather-local-vars body (append positional optional)))
+    (when local-vars
+      (setq body `((let ,local-vars ,@body))))
+
+    `(,(append (reverse positional) (if optional '(&optional) nil) (reverse optional))
+      ,@(reverse defaults)
+      ,@body)))
 
 (defmacro define (form &rest body)
   (if (consp form)
-      (let ((fn-name (car form))
-            (args (cdr form))
-            local-vars name positional optional defaults)
-        (dolist (a args)
-          (if (vectorp a)
-              (progn
-                (assert (= (length a) 2))
-                (setq name (aref a 0))
-                (assert (symbolp name))
-                (push name optional)
-                (push `(setq ,name (or ,name ,(aref a 1))) defaults))
-            (assert (symbolp a))
-            (push a positional)))
-        (setq local-vars (racket-gather-local-vars body (append positional optional)))
-        (when local-vars
-          (setq body `((let ,local-vars ,@body))))
-
-        `(defun ,fn-name ,(append (reverse positional) (if optional '(&optional) nil) (reverse optional))
-           ,@(reverse defaults)
-           ,@body))
+      (cons 'defun (cons (car form)  (racket-make-define-body form (racket-translate-define-body body))))
     (assert (symbolp form))
     `(defvar ,form ,body)))
 
