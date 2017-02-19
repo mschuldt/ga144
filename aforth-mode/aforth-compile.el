0ec714ff6ba08dbf0fb0bb712ce7fdc9645f7635
mschuldt <mbschuldt@gmail.com>
2017-02-19 15:27:48 -0800

    new elisp compilation functions

--- aforth-mode/aforth-compile.el
+++ aforth-mode/aforth-compile.el
@@ -38,6 +38,74 @@
     (insert code)
     (aforth-compile-buffer)))
 
+(setq _last-read-tok nil)
+
+(defun read-tok ()
+  (when current-token-list
+    (let ((token (car current-token-list))
+          token-type)
+      (set! current-token-list (cdr current-token-list))
+      (when token
+        (setq prev-current-tok-line current-tok-line
+              prev-current-tok-col current-tok-col
+              current-tok-line (aforth-token-start token) ;;TODO: line/col instead of buffer positions
+              current-tok-col (aforth-token-end token)))
+      (setq _last-read-tok token)
+      token)))
+
+(defun unread-last-tok ()
+  (assert elisp?)
+  (when _last-read-tok
+    (setq current-token-list (cons _last-read-tok current-token-list))))
+
+(defun read-tok-name ()
+  (let ((tok (read-tok)))
+    (and tok (aforth-token-value tok))))
+
+;;(fset 'rkt-compile-token 'compile-token)
+
+(defun compile-token (token)
+  (let ((token-type (aforth-token-type token))
+        (token-val (aforth-token-value token))
+        (token-args (aforth-token-args token))
+        fn)
+    (setq current-token token)
+    (when DEBUG? (printf "compile-token(~a) [~a  ~a  ~a]\n"
+                         (token-tok token) current-addr current-slot next-addr))
+    (cond ((or (eq token-type 'word-def)
+               (eq token-type 'compile-def))
+           (assert (setq fn (get-directive token-type)))
+           (funcall fn token-val))
+
+          ((or (eq token-type 'directive)
+               (eq token-type 'boot-descriptor))
+           (assert (setq fn (get-directive token-val)))
+           (apply fn token-args))
+
+          ((setq fn (get-directive token-type))
+           (fn))
+
+          ((eq token-type 'op)
+           (compile-instruction! token-val))
+
+          ((eq token-type 'number)
+           (compile-constant! token-val))
+
+          ((eq token-type 'r-call)
+           (compile-remote-call! token-val token-args))
+
+          ((eq token-type 'r-reference)
+           (error "TODO: compiling remote references")
+           )
+          ((eq token-type 'reference)
+           (compile-remote-word-ref! token-val token-args))
+
+          ((eq token-type 'call)
+           (compile-call! token-val))
+
+          (t (error "unrecognized token type: %s" token)))
+    (setq current-token nil)))
+
 (defun aforth-compile-file (filename)
   (with-temp-buffer
     (insert-file-contents-literally filename)
