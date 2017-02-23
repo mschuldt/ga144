5dd9dae7264c2e24fa44c8990b021c3badcb3543
mschuldt <mbschuldt@gmail.com>
2017-02-22 23:59:25 -0800

    compiler tests

---
+++ tests/test-compiler.rkt
@@ -0,0 +1,44 @@
+#lang racket ;; -*- lexical-binding: t -*-
+
+(require compatibility/defmacro
+         "../common.rkt"
+         "../compiler/compile.rkt"
+         "../compiler/assemble.rkt"
+         "../el.rkt")
+
+(define compiler-tests
+  '(("node 1 " (1 ))
+    ("node 1 2" (1 ("@p" "." "." ".") 2))
+    ("node 1 22 +" (1 ("@p" "+" "." ".") 22))
+    ("node 1 @ @ @ @ @ @" (1 ("@" "@" "@" ".") ("@" "@" "@" ".")))
+    ;;("node 1 " (1 ))
+    ))
+
+(define (trim-mem mem)
+  (map (lambda (x) (if (vector? x) (vector->list x) x))
+       (filter (lambda (x) (not (or (equal? x (vector false false false false))
+                                    (equal? x #f))))
+               (vector->list mem))))
+
+
+(define (run-compiler-tests)
+  (define code false)
+  (define compiled false)
+  (define compiled-hash false)
+  (define node false)
+  (define expect false)
+  (define mem false)
+  (for ((test compiler-tests))
+    (set! code (car test))
+    (assert (string? code))
+    (set! compiled (aforth-compile code))
+    (set! compiled-hash (make-hash))
+    (for ((node (compiled-nodes compiled)))
+      (hash-set! compiled-hash (node-coord node)
+                 (trim-mem (vector-copy (node-mem node)))))
+    (for ((x (cdr test)))
+      (set! node (car x))
+      (set! expect (cdr x))
+      (set! mem (hash-ref compiled-hash node))
+      (when (not (equal? mem expect))
+        (printf "failed '~a', got ~a, expected ~a " code mem expect)))))
